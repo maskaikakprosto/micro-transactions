d1e47a417e641eaec42c606c0c6200bc9190e8a9
Maksim Tatarinov <161529520+maskaikakprosto@users.noreply.github.com>
2024-02-28 00:39:09 +0200

    Create resourses.go

---
+++ resourses.go
@@ -0,0 +1,20 @@
+			accs, err := authtypes.UnpackAccounts(authGenState.Accounts)
+			if err != nil {
+				return fmt.Errorf("failed to get accounts from any: %w", err)
+			}
+
+			if accs.Contains(addr) {
+				return fmt.Errorf("cannot add account at existing address %s", addr)
+			}
+
+
+func TestInitCmd(t *testing.T) {
+	rootCmd, _ := cmd.NewRootCmd()
+	rootCmd.SetArgs([]string{
+		"init",        // Test the init cmd
+		"simapp-test", // Moniker
+		fmt.Sprintf("--%s=%s", cli.FlagOverwrite, "true"), // Overwrite genesis.json, in case it already exists
+	})
+
+	require.NoError(t, svrcmd.Execute(rootCmd, "", simapp.DefaultNodeHome))
+}
